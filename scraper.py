a3cf7abe68a7e071edfc5715e848e5cbee21ec6f
Robert England <46442441+RobertEngland@users.noreply.github.com>
2019-02-26 12:02:35 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,14 +7,24 @@ import lxml.html
 # # Read in a page
 html = scraperwiki.scrape("https://www.sdlauctions.co.uk/property-list/")
 # Print the variable html containing the webpage
-print(html)
+##commented out the print link so running the page doesn't take ages!
+#print(html)
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
 # inspecting the all the info in the <a tag within the <p> and <li> tags (P and LI are parents of A)
 root.cssselect("li p a")
 # then store the matched links in 'matchedlinks'
 matchedlinks = root.cssselect("li p a")
-print(matchedlinks)
+##commented out the print link so running the page doesn't take ages!
+# print(matchedlinks)
+# It's time to loop through the confusing codes from the page <Element a at 0x7f3fb1c536d8> calling each one li
+# We're calling it li because it relates to the tag we've grabbed it from
+for li in matchedlinks:
+  #store the text elements of li in a new variable showing just this'listtext'
+  listtest = li.text_content()
+  # print that
+  print(listext)
+  
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
